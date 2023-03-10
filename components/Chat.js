bec1a2be379079f640ab75da32430c5055798050
CetO-hub <102421594+CetO-hub@users.noreply.github.com>
2023-03-10 13:37:46 +0100

    docs: update readme

--- components/Chat.js
+++ components/Chat.js
@@ -5,10 +5,14 @@ import {
   StyleSheet,
   KeyboardAvoidingView,
   Platform,
+  Button,
 } from "react-native";
 import { GiftedChat, Bubble, InputToolbar } from "react-native-gifted-chat";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import NetInfo from "@react-native-community/netinfo";
+import CustomActions from "./CustomActions";
+import MapView from "react-native-maps";
+import { ActionSheetProvider } from "@expo/react-native-action-sheet";
 
 const firebase = require("firebase");
 require("firebase/firestore");
@@ -20,6 +24,8 @@ export default class Chat extends React.Component {
       messages: [],
       user: {},
       isConnected: false,
+      image: null,
+      location: null,
     };
     const firebaseConfig = {
       apiKey: "AIzaSyAK14AAznVyLEUVOgZiOJPEzNPBCtt9MVs",
@@ -150,6 +156,8 @@ export default class Chat extends React.Component {
           _id: data.user._id,
           name: data.user.name,
         },
+        image: data.image || null,
+        location: data.location || null,
       });
     });
     this.setState({ messages });
@@ -167,6 +175,8 @@ export default class Chat extends React.Component {
         _id: this.state.user._id,
         name: this.props.route.params.name,
       },
+      image: message[0].image || null,
+      location: message[0].location || null,
     });
   };
 
@@ -213,27 +223,53 @@ export default class Chat extends React.Component {
     );
   }
 
-  render() {
-    return (
-      <View
-        style={[
-          styles.container,
-          { backgroundColor: this.props.route.params.color },
-        ]}
-      >
-        <GiftedChat
-          renderBubble={this.renderBubble.bind(this)}
-          renderInputToolbar={this.renderInputToolbar.bind(this)}
-          messages={this.state.messages}
-          onSend={(messages) => this.onSend(messages)}
-          user={{
-            _id: this.state.user._id,
+  renderCustomActions = (props) => {
+    return <CustomActions {...props} />;
+  };
+
+  renderCustomView(props) {
+    const { currentMessage } = props;
+    if (currentMessage.location) {
+      return (
+        <MapView
+          style={{ width: 150, height: 100, borderRadius: 13, margin: 3 }}
+          region={{
+            latitude: currentMessage.location.latitude,
+            longitude: currentMessage.location.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
           }}
         />
-        {Platform.OS === "android" && (
-          <KeyboardAvoidingView behavior="height" />
-        )}
-      </View>
+      );
+    }
+    return null;
+  }
+
+  render() {
+    return (
+      <ActionSheetProvider>
+        <View
+          style={[
+            styles.container,
+            { backgroundColor: this.props.route.params.color },
+          ]}
+        >
+          <GiftedChat
+            renderBubble={this.renderBubble.bind(this)}
+            renderInputToolbar={this.renderInputToolbar.bind(this)}
+            messages={this.state.messages}
+            onSend={(messages) => this.onSend(messages)}
+            renderActions={this.renderCustomActions.bind(this)}
+            renderCustomView={this.renderCustomView.bind(this)}
+            user={{
+              _id: this.state.user._id,
+            }}
+          />
+          {Platform.OS === "android" && (
+            <KeyboardAvoidingView behavior="height" />
+          )}
+        </View>
+      </ActionSheetProvider>
     );
   }
 }
